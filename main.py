4acfc95de1cea0d11a0c2f1914984324bedfd42b
william-thu <59815676+william-thu@users.noreply.github.com>
2023-11-17 02:39:33 +0000

    setup

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-'''
-Responsible Team Members:
+"""
+---------------------------------------------------
+Team Member(s) contribution:
 
-'''
+- William:
+-
+---------------------------------------------------
+"""
 
 # ---------------------------- main.py - ---------------------------
 import traveller
 import hotel
 import flight
-import excursion
 import car_hire
+
+class HolidayBooking:
+    def __init__(self):
+        self.travellers = []
+        self.flights = []
+        self.car_hires = []
+
+    def obtain_details(self):
+        pass
+
+    def print_details(self):
+        pass
+
+    def calc_total_cost(self):
+        pass
+
+    def book_holiday(self):
+        print("--------------- Holiday Booked! ---------------")
+        print("Total cost of holiday: £")
+
+
 
